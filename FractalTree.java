29960befcdde41f18f2a99fedb99aab7b1165cbd
thekevingu <55099415+thekevingu@users.noreply.github.com>
2019-09-23 20:46:29 -0400

    Updated with random fractal trees

--- FractalTree.java
+++ FractalTree.java
@@ -1,5 +1,7 @@
 import edu.princeton.cs.algs4.*;
 
+import java.util.Random;
+
 /* KEVIN'S FRACTALTREE.JAVA */
 public class FractalTree {
 
@@ -10,31 +12,47 @@ public class FractalTree {
     private static final int angle = 0;
     private static final int currentOrder = 0;
 
+    /* RANDOM */
+    private static final Random r = new Random();
+
     public static void main(String[] args) {
         StdOut.println("Kevin's FractalTree.java\n");
 
         StdOut.print("Enter order: ");
         int order = StdIn.readInt();
 
-        drawTree(x, y, length, angle, order, currentOrder);
+        int branches = 4;
+        int[] angleChanges = new int[branches];
+        for (int i = 0; i < angleChanges.length; i++) {
+            angleChanges[i] = 30 + r.nextInt(15);
+        }
+
+        drawTree(x, y, length, angle, order, currentOrder, angleChanges, branches);
 
         StdOut.println("\nA tree was just generated.");
     }
 
     /* DRAWS TREE */
-    private static void drawTree(double x, double y, double length, int angle, int order, int currentOrder) {
+    private static void drawTree(double x, double y, double length, int angle, int order, int currentOrder, int[] angleChanges, int branches) {
         StdDraw.setPenColor(StdDraw.GRAY);
 
         if (currentOrder == 0)
             StdDraw.line(x, y, x + length * Math.cos(Math.toRadians(angle - 90)), y + length * Math.sin(Math.toRadians(angle - 90)));
 
         if (currentOrder < order) {
-            for (int i = 45; i <= 135; i += 15) {
+            for (int j = 0; j < branches; j++) {
+                int i = angleChanges[0];
+
+                for (int l = 0; l < j; l++)
+                    i += angleChanges[l];
+                
                 if (order - 1 == currentOrder)
                     StdDraw.setPenColor(StdDraw.BOOK_BLUE);
+
                 StdDraw.line(x, y, x + length * Math.cos(Math.toRadians(angle + i)), y + length * Math.sin(Math.toRadians(angle + i)));
-                drawTree(x + length * Math.cos(Math.toRadians(angle + i)), y + length * Math.sin(Math.toRadians(angle + i)), length / 2, angle + i - 90, order, currentOrder + 1);
+                drawTree(x + length * Math.cos(Math.toRadians(angle + i)), y + length * Math.sin(Math.toRadians(angle + i)), r.nextDouble() * length / 2, (int) r.nextDouble() * angle + i, order, currentOrder + 1, angleChanges, branches);
             }
         }
     }
 }
+
