5af0e7221996bd26b9790b330a6fc6988e2ae0c5
thekevingu <55099415+thekevingu@users.noreply.github.com>
2019-09-23 20:03:14 -0400

    Added comments

--- EveryVoteCounts.java
+++ EveryVoteCounts.java
@@ -6,10 +6,11 @@ import edu.princeton.cs.algs4.*;
 public class EveryVoteCounts {
 
     /* BLOCK VALUES AND THRESHOLD */
-    private static final int[] ALL_BUT_N = new int[]{5, 2, 6, 7, 3};
-    private static final int N = 4;
-    private static final int THRESHOLD = (IntStream.of(ALL_BUT_N).sum() + N) / 2 + 1;
+    private static final int[] ALL_BUT_N = new int[]{5, 2, 6, 7, 3, 4};
+    private static final int blockIndex = 4;
+    private static final int THRESHOLD = (IntStream.of(ALL_BUT_N).sum() + ALL_BUT_N[blockIndex]) / 2 + 1;
 
+    /* MAIN METHOD */
     public static void main(String[] args) {
         StdOut.println("Kevin's EveryVoteCounts.java\n");
 
@@ -18,11 +19,23 @@ public class EveryVoteCounts {
             StdOut.print(ALL_BUT_N[i] + " ");
         StdOut.println("\n");
         StdOut.print("N block has the following value: ");
-        StdOut.println(N + "\n");
+        StdOut.println(ALL_BUT_N[blockIndex] + "\n");
 
-        StdOut.println("Critical vote possibilities for N: " + countCriticalVotes(ALL_BUT_N, N));
+        StdOut.println("Critical vote possibilities for N: " + countCriticalVotesWrapperFunction(ALL_BUT_N, blockIndex));
     }
 
+    /* WRAPPER METHOD */
+    private static int countCriticalVotesWrapperFunction(int[] blocks, int blockIndex) {
+        int[] temp = new int[blocks.length - 1];
+        for (int i = 0; i < blocks.length; i++)
+            if (i < blockIndex)
+                temp[i] = blocks[i];
+            else if (i > blockIndex)
+                temp[i - 1] = blocks[i];
+        return countCriticalVotes(temp, blocks[blockIndex]);
+    }
+
+    /* COUNT CRITICAL VOTE RECURSIVE */
     private static int countCriticalVotes(int[] blocks, int blockCount) {
         int total = 0;
         int currentSum = IntStream.of(blocks).sum();
@@ -42,6 +55,7 @@ public class EveryVoteCounts {
         return total;
     }
 
+    /* RETURNS SUB-BLOCKS */
     private static int[] subBlocks(int[] blocks) {
         int[] returned = new int[blocks.length - 1];
 
@@ -51,6 +65,7 @@ public class EveryVoteCounts {
         return returned;
     }
 
+    /* RETURNS SKIPPING-BLOCK */
     private static int[] skipBlocks(int[] blocks) {
         int[] returned = new int[blocks.length - 1];
 
