a93b7f5882e0e418ec35a9f38aace71d5200d03a
thekevingu <55099415+thekevingu@users.noreply.github.com>
2019-10-01 21:28:20 -0400

    Updated version (comments ui)

--- EveryVoteCounts.java
+++ EveryVoteCounts.java
@@ -6,26 +6,59 @@ import edu.princeton.cs.algs4.*;
 public class EveryVoteCounts {
 
     /* BLOCK VALUES AND THRESHOLD */
-    private static final int[] ALL_BUT_N = new int[]{4, 7, 2, 4};
-    private static final int blockIndex = 1;
-    private static final int THRESHOLD = IntStream.of(ALL_BUT_N).sum() / 2 + 1;
+    private static int[] allBlocks;
+    private static int blockIndex;
+    private static int threshold;
 
     /* MAIN METHOD */
     public static void main(String[] args) {
         StdOut.println("Kevin's EveryVoteCounts.java\n");
 
-        StdOut.println("N-1 blocks have the following values:");
-        for (int i = 0; i < ALL_BUT_N.length; i++)
-            StdOut.print(ALL_BUT_N[i] + " ");
-        StdOut.println("\n");
-        StdOut.print("N block has the following value: ");
-        StdOut.println(ALL_BUT_N[blockIndex] + "\n");
+        StdOut.println("Enter number of blocks.");
+        int blocks = StdIn.readInt();
+        allBlocks = new int[blocks];
 
-        StdOut.println("Critical vote possibilities for N method 1: " + countCriticalVotesWrapperFunction(ALL_BUT_N, blockIndex));
-        StdOut.println("Critical vote possibilities for N method 2: " + countCriticalVotesWrapperFunction2(ALL_BUT_N, blockIndex));
+        StdOut.println("\nEnter block values:");
+        int entered = 0;
+        while (entered < blocks) {
+            int read = StdIn.readInt();
+            allBlocks[entered] = read;
+            entered++;
+        }
+
+        StdOut.println("\nEnter block index:");
+        blockIndex = StdIn.readInt();
+        threshold = IntStream.of(allBlocks).sum() / 2 + 1;
+
+        if (blockIndex >= 0 && blockIndex < blocks) {
+            StdOut.println("\nCritical vote possibilities for N using recursive method 1: " + countCriticalVotesWrapperFunction(allBlocks, blockIndex));
+            StdOut.println("Critical vote possibilities for N using recursive method 2: " + countCriticalVotesWrapperFunction2(allBlocks, blockIndex));
+        } else {
+            StdOut.println("\nError: block index out of bounds.");
+        }
     }
 
-    /* WRAPPER METHOD */
+    /* WRAPPER METHOD 1 */
+    private static int countCriticalVotesWrapperFunction2(int[] blocks, int blockIndex) {
+        int[] temp = new int[blocks.length - 1];
+        for (int i = 0; i < blocks.length; i++)
+            if (i < blockIndex)
+                temp[i] = blocks[i];
+            else if (i > blockIndex)
+                temp[i - 1] = blocks[i];
+        return countCriticalVotes2(temp, blocks[blockIndex], 0, 0, 0);
+    }
+
+    /* COUNT CRITICAL VOTE FUNCTION 1 */
+    private static int countCriticalVotes2(int[] blocks, int blockValue, int currentIndex, int currentSum, int critical) {
+        if (currentIndex == blocks.length) {
+            if (currentSum < threshold && currentSum + blockValue >= threshold) return critical + 1;
+        } else if (currentIndex < blocks.length)
+            return countCriticalVotes2(blocks, blockValue, currentIndex + 1, currentSum + blocks[currentIndex], critical) + countCriticalVotes2(blocks, blockValue, currentIndex + 1, currentSum, critical);
+        return 0;
+    }
+
+    /* WRAPPER METHOD 2 */
     private static int countCriticalVotesWrapperFunction(int[] blocks, int blockIndex) {
         int[] temp = new int[blocks.length - 1];
         for (int i = 0; i < blocks.length; i++)
@@ -36,12 +69,12 @@ public class EveryVoteCounts {
         return countCriticalVotes(temp, blocks[blockIndex]);
     }
 
-    /* COUNT CRITICAL VOTE RECURSIVE */
+    /* COUNT CRITICAL VOTE FUNCTION 2 */
     private static int countCriticalVotes(int[] blocks, int blockCount) {
         int total = 0;
         int currentSum = IntStream.of(blocks).sum();
 
-        if (currentSum < THRESHOLD && blockCount + currentSum >= THRESHOLD)
+        if (currentSum < threshold && blockCount + currentSum >= threshold)
             total++;
 
         if (blocks.length > 0) {
@@ -54,26 +87,6 @@ public class EveryVoteCounts {
             }
         }
         return total;
-    }
-
-    /* WRAPPER METHOD 2 */
-    private static int countCriticalVotesWrapperFunction2(int[] blocks, int blockIndex) {
-        int[] temp = new int[blocks.length - 1];
-        for (int i = 0; i < blocks.length; i++)
-            if (i < blockIndex)
-                temp[i] = blocks[i];
-            else if (i > blockIndex)
-                temp[i - 1] = blocks[i];
-        return countCriticalVotes2(temp, blocks[blockIndex], 0, 0, 0);
-    }
-
-    /* COUNT CRITICAL VOTE FUNCTION 2 */
-    private static int countCriticalVotes2(int[] blocks, int blockValue, int currentIndex, int currentSum, int critical) {
-        if (currentIndex == blocks.length) {
-            if (currentSum < THRESHOLD && currentSum + blockValue >= THRESHOLD) return critical + 1;
-        } else if (currentIndex < blocks.length)
-            return countCriticalVotes2(blocks, blockValue, currentIndex + 1, currentSum + blocks[currentIndex], critical) + countCriticalVotes2(blocks, blockValue, currentIndex + 1, currentSum, critical);
-        return 0;
     }
 
     /* RETURNS SUB-BLOCKS */
